4660ffd2794a596fc9e69cfd9bea91bb3d23fee7
Muktar Yusuf <mayunited12@yahoo.com>
2022-09-26 19:14:29 +0100

    view register

--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,14 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/inbox',
+    name: 'inbox',
+    component: () => import('../views/Inbox.vue')
+  },
+  {
+    path: '/archive',
+    name: 'archive',
+    component: () => import('../views/Inbox.vue')
   }
 ]
 
