3ea428562a0fca1f728a5e5f394a11b5de2e70ae
Muktar Yusuf <mayunited12@yahoo.com>
2022-09-26 21:09:59 +0100

    keystroke event

--- src/views/Archive.vue
+++ src/views/Archive.vue
@@ -93,6 +93,17 @@ export default {
       }
     },
   },
+  created() {
+    window.addEventListener('keydown', (e) => {
+      if (!this.selections.length) return
+
+      if (e.key == 'r') {
+        this.markSelectionAsRead()
+      } else if (e.key == 'a') {
+        this.markSelectionAsArchive()
+      }
+    });
+  }
 };
 </script>
 
