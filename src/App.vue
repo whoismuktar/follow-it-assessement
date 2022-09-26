3ea4149885c5009490137b5414291325f9a071a2
Muktar Yusuf <mayunited12@yahoo.com>
2022-09-26 19:14:36 +0100

    sidebar progress

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <side-bar />
   <router-view/>
 </template>
+
+<script>
+import SideBar from './components/SideBar'
+
+export default {
+  name: "sidebar",
+  components: { SideBar },
+}
+</script>
+
+<style src="@/assets/css/style.css"></style>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="sidebar">
+    <div class="sidebar__wrapper">
+      <div
+        v-for="(menu, index) in menuItems"
+        :key="index"
+        class="sidebar__wrapper__item"
+        :class="[`sidebar__wrapper__item__${menu.id}`]"
+      >
+        {{ menu.title }}
+      </div>
+
+      <div class="spacer"></div>
+
+      <div
+        class="sidebar__wrapper__item sidebar__wrapper__item__logout"
+      >
+        Logout
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "sidebar",
+  data() {
+    return {
+      menuItems: [
+        {
+          title: "Inbox",
+          path: "inbox",
+          id: "inbox",
+        },
+        {
+          title: "Archive",
+          path: "archive",
+          id: "archive",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.sidebar {
+  background: #373636;
+  width: 300px;
+  height: 100vh;
+  padding: 20px;
+  position: fixed;
+  left: 0;
+  top: 0;
+}
+.sidebar__wrapper {
+    height: 90%;
+    display: flex;
+    flex-direction: column;
+}
+.sidebar__wrapper__item {
+    color: #373636;
+    cursor: pointer;
+    background-color: #ffffff;
+    padding: 12px 10px;
+    border-radius: 10px;
+    margin-bottom: 20px;
+    font-weight: bold;
+}
+
+.sidebar__wrapper__item__logout {
+    color: #373636;
+    cursor: pointer;
+    background-color: #ffffff;
+    padding: 12px 10px;
+    border-radius: 10px;
+    margin-bottom: 20px;
+    font-weight: bold;
+}
+</style>
